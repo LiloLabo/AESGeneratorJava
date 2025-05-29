18a4aea29af2e3a8958ea630ca32c506db9349c9
LiloLabo <lilo.suryo@gmail.com>
2025-05-29 18:28:42 +0700

    remove comment

--- src/app/AESCBC.java
+++ src/app/AESCBC.java
@@ -1,42 +1,4 @@
-/*
- * Copyright (C) Global Accounting
- * http://www.marstech.co.id
- * Email. info@marstech.co.id
- * Telp. 0811-3636-09
- * Office        : Jl. Bulu Mas II No. 1 Kanigoro Kartoharjo Kota Madiun - Jawa Timur
- * Branch Office : Perum Griya Gadang Sejahtera Kav. 14 Gadang - Sukun - Kota Malang - Jawa Timur
- * 
- * Global Accounting
- * Adalah merek dagang dari PT. Marstech Global
- * 
- * License Agreement
- * Software komputer atau perangkat lunak komputer ini telah diakui sebagai salah satu aset perusahaan yang bernilai.
- * Di Indonesia secara khusus,
- * software telah dianggap seperti benda-benda berwujud lainnya yang memiliki kekuatan hukum.
- * Oleh karena itu pemilik software berhak untuk memberi ijin atau tidak memberi ijin orang lain untuk menggunakan softwarenya.
- * Dalam hal ini ada aturan hukum yang berlaku di Indonesia yang secara khusus melindungi para programmer dari pembajakan software yang mereka buat,
- * yaitu diatur dalam hukum hak kekayaan intelektual (HAKI).
- * 
- * ********************************************************************************************************
- * Pasal 72 ayat 3 UU Hak Cipta berbunyi,
- * " Barangsiapa dengan sengaja dan tanpa hak memperbanyak penggunaan untuk kepentingan komersial "
- * " suatu program komputer dipidana dengan pidana penjara paling lama 5 (lima) tahun dan/atau "
- * " denda paling banyak Rp. 500.000.000,00 (lima ratus juta rupiah) "
- * ********************************************************************************************************
- * 
- * Proprietary Software
- * Adalah software berpemilik, sehingga seseorang harus meminta izin serta dilarang untuk mengedarkan,
- * menggunakan atau memodifikasi software tersebut.
- * 
- * Commercial software
- * Adalah software yang dibuat dan dikembangkan oleh perusahaan dengan konsep bisnis,
- * dibutuhkan proses pembelian atau sewa untuk bisa menggunakan software tersebut.
- * Detail Licensi yang dianut di software https://en.wikipedia.org/wiki/Proprietary_software
- * EULA https://en.wikipedia.org/wiki/End-user_license_agreement
- * 
- * Lisensi Perangkat Lunak https://id.wikipedia.org/wiki/Lisensi_perangkat_lunak
- * EULA https://id.wikipedia.org/wiki/EULA
- */
+
 package app;
 
 import java.math.BigInteger;
@@ -52,12 +14,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
-/**
- *
- * @author LILO | lilo.suryo@gmail.com Created : 20 October 2020 - 23:23:39
- * Version : 1.0
- *
- */
 public class AESCBC {
 
     private String SECRET_KEY = "";
--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,43 +1,4 @@
-/*
- * Copyright (C) PT. Marstech Global
- * http://www.marstech.co.id
- * Email. info@marstech.co.id
- * Telp. 0811-3636-09
- * Office        : Jl. Bulu Mas II No. 1 Kanigoro Kartoharjo Kota Madiun - Jawa Timur
- * Branch Office : Perum Griya Gadang Sejahtera Kav. 14 Gadang - Sukun - Kota Malang - Jawa Timur
- *
- * Global Accounting
- * Adalah merek dagang dari PT. Marstech Global
- * 
- * Licensi Agreement
- * Software komputer atau perangkat lunak komputer ini telah diakui sebagai salah satu aset perusahaan yang bernilai.
- * Di Indonesia secara khusus,
- * software telah dianggap seperti benda-benda berwujud lainnya yang memiliki kekuatan hukum.
- * Oleh karena itu pemilik software berhak untuk memberi ijin atau tidak memberi ijin orang lain untuk menggunakan softwarenya.
- * Dalam hal ini ada aturan hukum yang berlaku di Indonesia yang secara khusus melindungi para programmer dari pembajakan software yang mereka buat,
- * yaitu diatur dalam hukum hak kekayaan intelektual (HAKI).
- *
- ********************************************************************************************************
- * Pasal 72 ayat 3 UU Hak Cipta berbunyi,                                                               *
- * \" Barangsiapa dengan sengaja dan tanpa hak memperbanyak penggunaan untuk kepentingan komersial  "/  *
- * \" suatu program komputer dipidana dengan pidana penjara paling lama 5 (lima) tahun dan/atau     "/  *
- * \" denda paling banyak Rp. 500.000.000,00 (lima ratus juta rupiah)                               "/  *
- ********************************************************************************************************
- *
- * Proprietary Software
- * Adalah software berpemilik, sehingga seseorang harus meminta izin serta dilarang untuk mengedarkan,
- * menggunakan atau memodifikasi software tersebut.
- *
- * Commercial software
- * Adalah software yang dibuat dan dikembangkan oleh perusahaan dengan konsep bisnis,
- * dibutuhkan proses pembelian atau sewa untuk bisa menggunakan software tersebut.
- * Detail Licensi yang dianut di software https://en.wikipedia.org/wiki/Proprietary_software
- * EULA https://en.wikipedia.org/wiki/End-user_license_agreement
- * 
- * Lisensi Perangkat Lunas https://id.wikipedia.org/wiki/Lisensi_perangkat_lunak
- * EULA https://id.wikipedia.org/wiki/EULA
- *
- */
+
 package app;
 
 import java.io.UnsupportedEncodingException;
@@ -56,11 +17,7 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.AnchorPane;
 
-/**
- *
- * @author LILO | lilo.suryo@gmail.com Created : 15 October 2020 - 09:25:41
- * Version : 1.0 Main Program
- */
+
 public class Controller extends AnchorPane implements Initializable {
 
     @FXML
